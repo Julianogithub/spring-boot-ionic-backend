72f6de6a624e004502a87fdb8069dbe404a8f3d2
Juliano Rocha <julianorocha.ti@gmail.com>
2020-04-22 23:09:47 -0300

    Pedido, Estado de pagamento( Cartao, boleto e Cancelamento de boleto ) * OK Console-h2 *

--- src/main/java/com/julianorocha/cursomc/domain/Cliente.java
+++ src/main/java/com/julianorocha/cursomc/domain/Cliente.java
@@ -16,7 +16,6 @@ import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.julianorocha.cursomc.domain.enums.TipoCliente;
-
 @Entity
 public class Cliente implements Serializable {
 	private static final long serialVersionUID = 1L;
@@ -36,6 +35,9 @@ public class Cliente implements Serializable {
 	@ElementCollection
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
+	
+	@OneToMany(mappedBy="cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
 	
 	public Cliente() {
 	}
@@ -105,6 +107,14 @@ public class Cliente implements Serializable {
 		this.telefones = telefones;
 	}
 
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -128,8 +138,5 @@ public class Cliente implements Serializable {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
-	}
-
-	
-
+	}	
 }
