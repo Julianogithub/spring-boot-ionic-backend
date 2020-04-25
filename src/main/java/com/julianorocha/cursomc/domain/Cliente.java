89d451512497188e6f7b2c9e0e8de27199d0cff4
Juliano Rocha <julianorocha.ti@gmail.com>
2020-04-25 11:11:17 -0300

    Endpoint /pedido/{id} disponibilizado com ( Polimorfismo de classes ) * OK *)

--- src/main/java/com/julianorocha/cursomc/domain/Cliente.java
+++ src/main/java/com/julianorocha/cursomc/domain/Cliente.java
@@ -14,8 +14,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.julianorocha.cursomc.domain.enums.TipoCliente;
+
 @Entity
 public class Cliente implements Serializable {
 	private static final long serialVersionUID = 1L;
@@ -36,6 +38,7 @@ public class Cliente implements Serializable {
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
+	@JsonBackReference
 	@OneToMany(mappedBy="cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
