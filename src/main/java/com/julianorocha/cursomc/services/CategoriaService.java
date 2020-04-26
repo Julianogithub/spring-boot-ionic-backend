55da298ab9e051558bb500d2f5cd865b866042f8
Juliano Rocha <julianorocha.ti@gmail.com>
2020-04-26 12:30:05 -0300

    Validacao sintatica com Bean Validation ( Categoria )* ok *

--- src/main/java/com/julianorocha/cursomc/services/CategoriaService.java
+++ src/main/java/com/julianorocha/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.julianorocha.cursomc.domain.Categoria;
+import com.julianorocha.cursomc.dto.CategoriaDTO;
 import com.julianorocha.cursomc.repositories.CategoriaRepository;
 import com.julianorocha.cursomc.services.exceptions.DataIntegrityException;
 import com.julianorocha.cursomc.services.exceptions.ObjectNotFoundException;
@@ -56,4 +57,7 @@ public class CategoriaService {
 		return repo.findAll(pageRequest);
 	}
 	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
+	}
 }
