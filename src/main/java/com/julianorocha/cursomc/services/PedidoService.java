5b8c75b3c81284b45356d7da427eb8d1e79c792b
Juliano Rocha <julianorocha.ti@gmail.com>
2020-05-02 15:19:57 -0300

    Implementando toString de Pedido * ok *

--- src/main/java/com/julianorocha/cursomc/services/PedidoService.java
+++ src/main/java/com/julianorocha/cursomc/services/PedidoService.java
@@ -15,7 +15,6 @@ import com.julianorocha.cursomc.domain.enums.EstadoPagamento;
 import com.julianorocha.cursomc.repositories.ItemPedidoRepository;
 import com.julianorocha.cursomc.repositories.PagamentoRepository;
 import com.julianorocha.cursomc.repositories.PedidoRepository;
-import com.julianorocha.cursomc.repositories.ProdutoRepository;
 import com.julianorocha.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -23,22 +22,22 @@ public class PedidoService {
 
 	@Autowired
 	private PedidoRepository repo;
-
+	
 	@Autowired
 	private BoletoService boletoService;
-
+	
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
-
+	
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
-
-	@SuppressWarnings("unused")
-	@Autowired
-	private ProdutoRepository produtoRepository;
-
+	
 	@Autowired
 	private ProdutoService produtoService;
+	
+	@Autowired
+	private ClienteService clienteService;
+	
 
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
@@ -50,6 +49,7 @@ public class PedidoService {
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
+		obj.setCliente(clienteService.find(obj.getCliente().getId()));
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
 		if (obj.getPagamento() instanceof PagamentoComBoleto) {
@@ -60,10 +60,13 @@ public class PedidoService {
 		pagamentoRepository.save(obj.getPagamento());
 		for (ItemPedido ip : obj.getItens()) {
 			ip.setDesconto(0.0);
-			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setProduto(produtoService.find(ip.getProduto().getId()));
+			ip.setPreco(ip.getProduto().getPreco());
 			ip.setPedido(obj);
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
+		System.out.println(obj);
 		return obj;
 	}
+
 }
